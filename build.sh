b4325e4e895b0ad445e6c273220dd63e27855c72
junbin <junbin@guazi.com>
2018-05-07 00:00:48 +0800

    buid.sh

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env
+go build -o output/markdown && output/markdown
--- main.go
+++ main.go
@@ -4,37 +4,41 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/url"
-	//"os"
+	"time"
 )
 
 var (
-	//method string
-	u string
+	method string
+	u      string
 )
 
 func main() {
-	// fmt.Println("enter method :")
-	// fmt.Scanln(&method)
-	// fmt.Println("method is :", method)
+	//p := fmt.Println
+	fmt.Println("enter method :")
+	fmt.Scanln(&method)
+	fmt.Println("method is :", method)
 	fmt.Println("enter url :")
 	fmt.Scanln(&u)
-	fmt.Println("url is :", u)
+	//fmt.Println("url is :", u)
 	res, err := url.Parse(u)
 	if err != nil {
 		fmt.Println("url parse faild .")
 	}
-	fmt.Println(res.Host)
-	fmt.Println(res.Path)
-	fmt.Println(res.RawQuery)
+	// fmt.Println(res.Host)
+	// fmt.Println(res.Path)
+	// fmt.Println(res.RawQuery)
 	query, _ := url.ParseQuery(res.RawQuery)
-	for k, v := range query {
-		fmt.Println(k, ":", v)
-	}
+	// for k, v := range query {
+	// 	fmt.Println(k, ":", v)
+	// }
 	hearder := "| 参数 | 备注 |\n|--- |---|\n"
 	for k, v := range query {
 		hearder = hearder + "|" + k + "|" + v[0] + "|\n"
 	}
-
+	hearder = hearder + "```json\n```"
 	//os.OpenFile("test.md", os.O_APPEND|os.O_CREATE|os.O_RDWR, 777)
-	ioutil.WriteFile("test.md", []byte(hearder), 0777)
+	t := time.Now()
+	file_name := []byte(t.Format(time.RFC3339))[0:10]
+	//fmt.Println(string(file_name))
+	ioutil.WriteFile("/Users/cn/Desktop/"+string(file_name)+".md", []byte(hearder), 0777)
 }
