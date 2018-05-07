a821a0b919223ff7bc6bd486fdd39979d6dcfe97
junbin <junbin@guazi.com>
2018-05-08 01:25:27 +0800

    0.0.2

---
+++ markdown-api.rb
@@ -0,0 +1,31 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class MarkdownApi < Formula
+  desc "markdown-api-desc"
+  homepage "https://github.com/cn100800/markdown-api/releases/latest"
+  url "https://github.com/cn100800/markdown-api/releases/download/0.0.2/markdown-api.zip"
+  version "0.0.2"
+  sha256 ""
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "cmake", ".", *std_cmake_args
+    bin.install "markdown"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test markdown-api`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
