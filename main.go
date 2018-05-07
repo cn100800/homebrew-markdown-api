940fdae2189010181d980add25ac6e6fcbf10b3d
junbin <junbin@guazi.com>
2018-05-07 18:30:48 +0800

    fix

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/url"
@@ -14,12 +15,22 @@ var (
 
 func main() {
 	//p := fmt.Println
-	fmt.Println("enter method :")
-	fmt.Scanln(&method)
-	fmt.Println("method is :", method)
-	fmt.Println("enter url :")
-	fmt.Scanln(&u)
-	//fmt.Println("url is :", u)
+	// -u -m -t
+	//
+	t := flag.String("t", "api title", "api title")
+	m := flag.String("m", "request method", "request method")
+	flag.StringVar(&u, "u", "request url", "request url")
+	flag.Parse()
+	// if *m == "" {
+	// 	fmt.Println("enter method :")
+	// 	fmt.Scanln(m)
+	// 	fmt.Println("method is :", m)
+	// }
+	// if u == "" {
+	// 	fmt.Println("enter url :")
+	// 	fmt.Scanln(&u)
+	// 	fmt.Println("url is :", u)
+	// }
 	res, err := url.Parse(u)
 	if err != nil {
 		fmt.Println("url parse faild .")
@@ -28,13 +39,13 @@ func main() {
 	// fmt.Println(res.Path)
 	// fmt.Println(res.RawQuery)
 	query, _ := url.ParseQuery(res.RawQuery)
-	hearder := "|method|path|\n|---|---|\n|" + method + "|" + res.Path + "|\n#query\n| 参数 | 备注 |\n|--- |---|\n"
+	hearder := "#" + *t + "\n|method|path|\n|---|---|\n|" + *m + "|" + res.Path + "|\n#query\n| 参数 | 备注 |\n|--- |---|\n"
 	for k, v := range query {
 		hearder = hearder + "|" + k + "|" + v[0] + "|\n"
 	}
-	hearder = hearder + "```json\n```"
+	hearder = hearder + "#result\n" + "```json\n```"
 	//os.OpenFile("test.md", os.O_APPEND|os.O_CREATE|os.O_RDWR, 777)
-	t := time.Now()
-	file_name := []byte(t.Format(time.RFC3339))[0:10]
+	t_now := time.Now()
+	file_name := []byte(t_now.Format(time.RFC3339))[0:10]
 	ioutil.WriteFile("/Users/cn/Desktop/"+string(file_name)+".md", []byte(hearder), 0777)
 }
